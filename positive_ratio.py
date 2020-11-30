22fac40745aff3bfe9e94c604f5f759dd020d65a
Marcin Lewandowski <marcin.lewandowski@avicon.pl>
2020-11-30 15:02:11 +0100

    2x3 suplots; txt inside figure; simplified moving_average; floats instead of ints

--- positive_ratio.py
+++ positive_ratio.py
@@ -1,3 +1,4 @@
+import datetime
 import requests
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,14 +17,14 @@ def get_tests_values(text):
     data = data2[2] + '-' + data2[1] + '-' + data2[0]
 
     if tests != 0:
-        ratio = 100 * float(positive) / float(tests)
+        ratio = 100.0 * float(positive) / float(tests)
     else:
-        ratio = 0
+        ratio = 0.0
 
     if tested_people != 0:
-        ratio_tested_people = 100 * positive / tested_people
+        ratio_tested_people = 100.0 * positive / tested_people
     else:
-        ratio_tested_people = 0
+        ratio_tested_people = 0.0
     print(data, tests, positive, ratio, ratio_tested_people)
 
     return text[text.find('},') + len('},'):], np.datetime64(data, 'D'), ratio, ratio_tested_people, tests, positive
@@ -48,17 +49,15 @@ def get_deaths_recovered_values(text):
     return text[text.find('},') + len('},'):], np.datetime64(data, 'D'), death_recovered_ratio, deaths
 
 
-def draw_plot(x_vals, y_vals, x_axis_label, y_axis_label, label):
-    fig, ax = plt.subplots(figsize=(6, 6))
+def draw_plot(x_vals, y_vals, x_axis_label: str, y_axis_label: str, label: str, ax: plt.Axes):
     ax.set_xlabel(x_axis_label)
     ax.set_ylabel(y_axis_label)
 
-    # data = [datas, ratios]
     ax.scatter(x_vals, y_vals, color='blue', s=4, label='Surowe dane')
-    ax.plot(x_vals, moving_average(5, y_vals), color='red', alpha=1, label='Średnia z 5 dni')
-    plt.axvline(x=np.datetime64('2020-11-04', 'D'), label='Konferencja ws lockdownu', c='black', linestyle=':')
-    plt.axvline(x=np.datetime64('2020-07-01', 'D'), label='Koronawirus "w odwrocie"', c='orange', linestyle=':')
-    plt.legend(bbox_to_anchor=(0., -0.4, 1., 0.), loc='lower left')
+    ax.plot(x_vals, moving_average(y_vals), color='red', alpha=1, label='Średnia z 5 dni')
+    ax.axvline(x=np.datetime64('2020-11-04', 'D'), label='Konferencja ws lockdownu', c='black', linestyle=':')
+    ax.axvline(x=np.datetime64('2020-07-01', 'D'), label='Koronawirus "w odwrocie"', c='orange', linestyle=':')
+    ax.legend(bbox_to_anchor=(0., -0.4, 1., 0.), loc='upper left')
 
     # format the ticks
     ax.xaxis.set_major_locator(dates.MonthLocator())
@@ -72,26 +71,12 @@ def draw_plot(x_vals, y_vals, x_axis_label, y_axis_label, label):
     datemin = np.datetime64(datas[0], 'D')
     datemax = np.datetime64(datas[-1], 'D') + np.timedelta64(3, 'D')
     ax.set_xlim(datemin, datemax)
-    fig.autofmt_xdate()
-    fig.suptitle(label)
-    plt.subplots_adjust(None, 0.27)
+    ax.set_title(label)
 
 
-def moving_average(step, arr):
-    averaged = []
-    for i, val in enumerate(arr):
-        if (i - step) > 0:
-            # print(arr[i-step:i+1])
-            subarray_avg = np.mean(arr[i - step:i + 1])
-        else:
-            # print(arr[0:i+1])
-            subarray_avg = np.mean(arr[0:i + 1])
 
-        averaged.append(subarray_avg)
-    # print(averaged)
-    # print(len(averaged))
-    # print(len(tests_array))
-    return averaged
+def moving_average(arr, step=5):
+    return np.convolve(arr, np.ones(step)/step, 'same')
 
 
 years = mdates.YearLocator()  # every year
@@ -110,7 +95,6 @@ ratios = [r]
 datas = [d]
 tests_array = [tests]
 new_cases = [positive]
-
 
 while len(text) > 10:
     text, d, r, r_tested_people, tests, positive = get_tests_values(text)
@@ -135,19 +119,34 @@ while len(text) > 10:
     datas2.append(d)
     death_daily.append(deaths)
 
-draw_plot(datas, ratios, 'Data', 'Udział wyników pozytywnych [%]', 'Stosunek testów pozytywnych do wszystkich testów')
-draw_plot(datas, tests_array, 'Data', 'Liczba testów', 'Liczba dziennie wykonanych testów')
-draw_plot(datas, new_cases, 'Data', 'Liczba zakażeń', 'Liczba dziennie wykrytych zakażeń')
+fig, axes = plt.subplots(figsize=(12, 12), nrows=3, ncols=2)
+draw_plot(datas, ratios, 'Data', 'Udział wyników pozytywnych [%]', 'Stosunek testów pozytywnych do wszystkich testów',
+          axes[0, 0])
+draw_plot(datas, tests_array, 'Data', 'Liczba testów', 'Liczba dziennie wykonanych testów', axes[0, 1])
+draw_plot(datas, new_cases, 'Data', 'Liczba zakażeń', 'Liczba dziennie wykrytych zakażeń', axes[1, 0])
 draw_plot(datas2, dr_ratios, 'Data', 'Liczba zmarłych / liczba wyleczonych',
-          'Stosunek liczby zmarłych do wyleczonych - dziennie')
+          'Stosunek liczby zmarłych do wyleczonych - dziennie', axes[1, 1])
 draw_plot(datas2, death_daily, 'Data', 'Liczba zgonów',
-          'Liczba zgonów - dziennie')
-print('_________________________________')
-print('Dane z', datas[-1])
-print('Liczba testów:', tests_array[-1])
-print('Liczba zakażeń:', new_cases[-1])
-print('Liczba zgonów:', death_daily[-1])
-print('Stosunek wyników pozytywnych:', ratios[-1])
-print('Stosunek zgonów do wyzdrowień:', dr_ratios[-1])
+          'Liczba zgonów - dziennie', axes[2, 0])
 
-plt.show()
+fig.autofmt_xdate()
+
+txt = (f'Dane z {datas[-1]}\n'
+       f'Liczba testów: {tests_array[-1]}\n'
+       f'Liczba zakażeń: {new_cases[-1]}\n'
+       f'Liczba zgonów: {death_daily[-1]}\n'
+       f'Stosunek wyników pozytywnych: {ratios[-1]:.3f}%\n'
+       f'Stosunek zgonów do wyzdrowień: {dr_ratios[-1]:.3f}\n'
+       )
+
+axes[2, 1].text(0.1, 0.9, txt, verticalalignment='top', transform=axes[2, 1].transAxes)
+
+#plt.subplots_adjust(None, 0.27)
+
+now = str(datetime.datetime.now())
+for c in [':', '.', '-']:
+    now = now.replace(c, '')
+now = now.replace(' ', '_')
+plt.savefig(f'corona_{now}.png', bbox_inches='tight')
+#plt.show()
+
